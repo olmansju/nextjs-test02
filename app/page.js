a64097dbc3dc52c033ba1f643caad7fe8f20b660
Justin Olmanson <olmansju@gmail.com>
2024-04-01 16:06:50 -0500

    added the vercel/postgres package to the project

--- app/page.js
+++ app/page.js
@@ -1,10 +1,36 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { sql } from '@vercel/postgres';
 
-export default function Home() {
+export default async function Home() {
+  const returned = await sql `SELECT * FROM pies;`;
+  let stringedReturn = JSON.stringify(returned.rows);
+  let dataArray = returned.rows;
+
+
   return (
   <>
     <br></br>Hello world, I need some food. I am a page connected to github and vercel. <p>Connecting it all together video 3.</p>
+    <br></br>
+    <br></br>
+    <table>
+    <thead>
+      <tr>
+        {dataArray.length > 0 && Object.keys(dataArray[0]).map((key) => (
+          <th key={key}>{key}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {dataArray.map((item) => (
+        <tr key={item.id}>
+          {Object.values(item).map((value, index) => (
+            <td key={index}>{value}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
     </>
   );
 }
