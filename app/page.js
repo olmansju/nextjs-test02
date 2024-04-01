929b435bf9b69aad6d85df64693a735da9f281a6
Justin Olmanson <olmansju@gmail.com>
2024-04-01 12:55:55 -0500

    text editing

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
   <>
-    <br></br>Hello world, I need some sleep. I am a page. <p>Connecting it all together video 2.</p>
+    <br></br>Hello world, I need some food. I am a page. <p>Connecting it all together video 2.</p>
     </>
   );
 }
